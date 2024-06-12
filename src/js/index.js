da129cd268ab0a4ac2e4255dd5cc8498d2ddbc4e
TiBaT1 <104798588+TiBaT1@users.noreply.github.com>
2024-06-12 16:36:38 -0300

    Update index.js

--- src/js/index.js
+++ src/js/index.js
@@ -1,13 +1,36 @@
 const btnAvancar = document.getElementById("btn-avancar");
+const btnVoltar = document.getElementById("btn-voltar");
 let cartaoAtual = 0;
+const cartoes = document.querySelectorAll(".cartao");
 
-btnAvancar.addEventListener("click", function(){
-  const cartaoSelecionado = document.querySelector(".selecionado");
-  cartaoSelecionado.classList.remove("selecionado")
+
+btnAvancar.addEventListener("click", function() {
+
+  if(cartaoAtual === cartoes.length - 1) let cartaoAtual = 0;
   
-  const cartoes = document.querySelectorAll(".cartao");
+  esconderCartaoSelecionado()
+  
   cartaoAtual++;
-  cartoes[cartaoAtual].classList.add("selecionado");
+  mostrarCartoes();
   
 })
 
+btnVoltar.addEventListener("click", function() {
+
+  if(cartaoAtual === 0) let cartaoAtual = 8;
+  
+  esconderCartaoSelecionado()
+  
+  cartaoAtual--;
+  mostrarCartao();
+  
+})
+
+function mostrarCartao() {
+  cartoes[cartaoAtual].classList.add("selecionado");
+}
+
+function esconderCartaoSelecionado () {
+  const cartaoSelecionado = document.querySelector(".selecionado");
+  cartaoSelecionado.classList.remove("selecionado")
+}
