9cd40a88ea5e509696118ae6e11c8cc542b579fb
TiBaT1 <104798588+TiBaT1@users.noreply.github.com>
2024-06-12 15:23:35 -0300

    Update index.js

--- src/js/index.js
+++ src/js/index.js
@@ -1,5 +1,13 @@
 const btnAvancar = document.getElementById("btn-avancar");
+let cartaoAtual = 0;
 
 btnAvancar.addEventListener("click", function(){
-  console.log("cliquei");
+  const cartaoSelecionado = document.querySelector(".selecionado");
+  cartaoSelecionado.classList.remove("selecionado")
+  
+  const cartoes = document.querySelectorAll(".cartao");
+  cartaoAtual++;
+  cartoes[cartaoAtual].classList.add("selecionado");
+  
 })
+
